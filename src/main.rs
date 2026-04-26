21263509bd9ff906d9ac06d10ae6fd130e103bec
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-04-26 21:57:18 +0200

    NOT working preassure reading

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 use arduino_hal::hal::clock::MHz8;
 
-use battery_free_climat_sensor::{drivers::veml7700::{config::ConfigFastLowPower, driver::Veml7700}, power_controlled_bus::ActiveLowPin};
+use battery_free_climat_sensor::{drivers::{bmp280::Bmp280, veml7700::{config::ConfigFastLowPower, driver::Veml7700}}, power_controlled_bus::ActiveLowPin};
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -51,11 +51,36 @@ fn main() -> ! {
         Err(e) => ufmt::uwrite!(&mut serial, "Unable to initialize VEML7700: \n{:?}\r\n", e).unwrap_infallible()
     }
 
+    let bmp280 = match Bmp280::init(&mut i2c, 0x77){
+        Ok(device) => {
+            ufmt::uwrite!(&mut serial, "Bmp280 initilized\r\n").unwrap_infallible();
+            Some(device)
+        },
+        Err(_) => {
+            ufmt::uwrite!(&mut serial, "Unable to initialize Bmp280\r\n").unwrap_infallible();
+            None
+        }
+    };
+
+    delay.delay_ms(100);
+
     loop {
         
         match veml7700.read(&mut i2c){
             Ok(lx) => ufmt::uwrite!(&mut serial, "Light sensor: {} lx\r\n", lx).unwrap_infallible(),
             Err(_) => ufmt::uwrite!(&mut serial, "Unable to read light sensor data\r\n").unwrap_infallible()
+        }
+
+        if let Some(device) = &bmp280{
+            ufmt::uwrite!(&mut serial, "- BMP280 data:\r\n").unwrap_infallible();
+
+            match device.read_data(&mut i2c){
+                Err(_) => ufmt::uwrite!(&mut serial, "--> Unable to read BMP280 data\r\n").unwrap_infallible(),
+                Ok(data) => {
+                    ufmt::uwrite!(&mut serial, "--> Temperature: {}m°C\r\n", data.temperature).unwrap_infallible();
+                    ufmt::uwrite!(&mut serial, "--> Pressure: {}Pa\r\n", data.pressure >> 8).unwrap_infallible();
+                }
+            }
         }
         
         delay.delay_ms(1000);
