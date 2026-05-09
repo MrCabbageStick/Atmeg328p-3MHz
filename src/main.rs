47267776cf0d5500248aaf440fdb9a432bae7382
daniel.wiszniowski <mrcabbagestick@gmail.com>
2026-05-09 17:23:41 +0200

    Add voltage divider calculations

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@
 #![no_main]
 #![feature(asm_experimental_arch)]
 
-use arduino_hal::{I2c, hal::{delay::Delay, usart::Usart}, prelude::{_unwrap_infallible_UnwrapInfallible}, usart::Baudrate};
+use arduino_hal::{Adc, I2c, hal::{delay::Delay, usart::Usart}, prelude::_unwrap_infallible_UnwrapInfallible, usart::Baudrate};
 use embedded_hal::delay::DelayNs;
 use panic_halt as _;
 use arduino_hal::hal::clock::MHz8;
 
-use battery_free_climat_sensor::{drivers::{bmp280::Bmp280, veml7700::{config::ConfigFastLowPower, driver::Veml7700}}, power_controlled_bus::ActiveLowPin};
+use battery_free_climat_sensor::{drivers::{bmp280::Bmp280, veml7700::{config::ConfigFastLowPower, driver::Veml7700}}, power_controlled_bus::ActiveLowPin, resistor_divider::read_voltage_divider_mv};
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut adc = Adc::new(dp.ADC, Default::default());
+
+    // Pins for reading capacitor voltage level
+    let mut capacitor_vsum_pin = pins.a1.into_analog_input(&mut adc);
+    let mut capacitor_halfv_pin = pins.a0.into_analog_input(&mut adc);
 
     // Enable power to all devices
     let mut en_vcc_i2c = ActiveLowPin::from_pin(pins.d8.into_output());
@@ -35,54 +40,16 @@ fn main() -> ! {
 
     let mut delay = Delay::<MHz8>::new();
 
-    let mut i2c = I2c::with_external_pullup(
-        dp.TWI, 
-        pins.a4.into_floating_input(), 
-        pins.a5.into_floating_input(), 
-        50_000
-    );
-
     ufmt::uwrite!(&mut serial, "--------------------------\r\n").unwrap_infallible();
 
-    let veml7700 = Veml7700::<ConfigFastLowPower>::new(0x10);
-    
-    match veml7700.init(&mut i2c){
-        Ok(_) => ufmt::uwrite!(&mut serial, "VEML7700 initialized\r\n").unwrap_infallible(),
-        Err(e) => ufmt::uwrite!(&mut serial, "Unable to initialize VEML7700: \n{:?}\r\n", e).unwrap_infallible()
-    }
-
-    let bmp280 = match Bmp280::init(&mut i2c, 0x77){
-        Ok(device) => {
-            ufmt::uwrite!(&mut serial, "Bmp280 initilized\r\n").unwrap_infallible();
-            Some(device)
-        },
-        Err(_) => {
-            ufmt::uwrite!(&mut serial, "Unable to initialize Bmp280\r\n").unwrap_infallible();
-            None
-        }
-    };
-
-    delay.delay_ms(100);
-
     loop {
+        let v_cap = read_voltage_divider_mv::<100_000,1_000_000,3300,_>(&mut capacitor_vsum_pin, &mut adc);
+        let v_half_cap = read_voltage_divider_mv::<1_000_000,100_000,3300,_>(&mut capacitor_halfv_pin, &mut adc);
         
-        match veml7700.read(&mut i2c){
-            Ok(lx) => ufmt::uwrite!(&mut serial, "Light sensor: {} lx\r\n", lx).unwrap_infallible(),
-            Err(_) => ufmt::uwrite!(&mut serial, "Unable to read light sensor data\r\n").unwrap_infallible()
-        }
+        ufmt::uwrite!(&mut serial, "V_cap: {}mV\r\n", v_cap).unwrap_infallible();
+        ufmt::uwrite!(&mut serial, "V_half_cap: {}mV\r\n", v_half_cap).unwrap_infallible();
+        ufmt::uwrite!(&mut serial, "---\r\n").unwrap_infallible();
 
-        if let Some(device) = &bmp280{
-            ufmt::uwrite!(&mut serial, "- BMP280 data:\r\n").unwrap_infallible();
-
-            match device.read_data(&mut i2c){
-                Err(_) => ufmt::uwrite!(&mut serial, "--> Unable to read BMP280 data\r\n").unwrap_infallible(),
-                Ok(data) => {
-                    ufmt::uwrite!(&mut serial, "--> Temperature: {}m°C\r\n", data.temperature).unwrap_infallible();
-                    ufmt::uwrite!(&mut serial, "--> Pressure: {}Pa\r\n", data.pressure >> 8).unwrap_infallible();
-                }
-            }
-        }
-        
         delay.delay_ms(1000);
     }
 }
